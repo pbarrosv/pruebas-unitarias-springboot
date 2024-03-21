db073ef6cfe1c1790a5aa012699d9a50d520e0d4
pbarrosv <pedroluisbarrosvalle@hotmail.com>
2024-03-21 11:31:37 -0500

    implementando los metodos de la clase EmpleadoServiceImpl

--- src/main/java/com/api/rest/test/service/EmpleadoServiceImpl.java
+++ src/main/java/com/api/rest/test/service/EmpleadoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.api.rest.test.service;
 
+import com.api.rest.test.exception.ResourceNotFoundException;
 import com.api.rest.test.models.Empleado;
 import com.api.rest.test.repository.EmpleadosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,26 +15,30 @@ public class EmpleadoServiceImpl implements EmpleadoService{
 
     @Override
     public Empleado saveEmpleado(Empleado empleado) {
+        Optional<Empleado> empleadoGuardado =  empleadosRepository.findByEmail(empleado.getEmail());
+        if (empleadoGuardado.isPresent()){
+            throw new ResourceNotFoundException("El empleado con ese email ya existe : " + empleado.getEmail());
+        }
         return (Empleado)empleadosRepository.save(empleado);
     }
 
     @Override
     public List<Empleado> getAllEmpleados() {
-        return null;
+        return empleadosRepository.findAll();
     }
 
     @Override
     public Optional<Empleado> getEmpleadoById(Long id) {
-        return Optional.empty();
+        return empleadosRepository.findById(id);
     }
 
     @Override
     public Empleado updateEmpleado(Empleado empleadoActualizado) {
-        return null;
+        return (Empleado) empleadosRepository.save(empleadoActualizado);
     }
 
     @Override
     public void deleteEmpleado(Long id) {
-
+        empleadosRepository.deleteById(id);
     }
 }
