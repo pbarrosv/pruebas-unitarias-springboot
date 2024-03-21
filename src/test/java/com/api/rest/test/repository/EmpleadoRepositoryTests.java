0a131a122061f5331c5f8e6698abea1e2bb49afc
pbarrosv <pedroluisbarrosvalle@hotmail.com>
2024-03-21 12:21:22 -0500

    Creacion de la clase EmpleadoRepositoryTests

---
+++ src/test/java/com/api/rest/test/repository/EmpleadoRepositoryTests.java
@@ -0,0 +1,9 @@
+package com.api.rest.test.repository;
+
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+
+@DataJdbcTest
+public class EmpleadoRepositoryTests {
+
+    
+}
