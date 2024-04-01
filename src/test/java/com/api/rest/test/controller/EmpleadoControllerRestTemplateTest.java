f3d632cd2ba220eeea53451f2ef36e55d45ea9b3
pbarrosv <pedroluisbarrosvalle@hotmail.com>
2024-04-01 15:13:44 -0500

    Se crea la clase EmpleadoControllerRestTemplateTest

--- src/test/java/com/api/rest/test/controller/EmpleadoControllerRestTemplateTest.java
+++ src/test/java/com/api/rest/test/controller/EmpleadoControllerRestTemplateTest.java
@@ -1,2 +1,35 @@
-package com.api.rest.test.controller;public class EmpleadoControllerRestTemplateTest {
+package com.api.rest.test.controller;
+
+
+import com.api.rest.test.models.Empleado;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) /*Vamos a indicar el orden de los metodos*/
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)/*Le decimos que usaremos un puerto aleatorio*/
+public class EmpleadoControllerRestTemplateTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    @Order(1)
+    void testGuardarEmpleado(){
+        Empleado empl = Empleado.builder()
+                .id(1L)
+                .nombre("Pedro")
+                .apellido("Barros")
+                .email("pbarros@gmail.com")
+                .build();
+        /*Vamos poner al empl y le vamos a poner de que tipo es Empleado.class*/
+        ResponseEntity<Empleado> respuesta = testRestTemplate.postForEntity("http://localhost:8080/api/empleados",empl,Empleado.class);
+    }
 }
